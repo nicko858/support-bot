5aad02089de202d108652fa107fbde20554d7b30
nicko858 <ikar2021@hotmail.com>
2020-03-22 20:55:34 +0300

    Complete test credentials in heroku env

--- vk_bot.py
+++ vk_bot.py
@@ -78,7 +78,7 @@ def text_message(vk_session, vk):
 if __name__ == '__main__':
     load_dotenv()
     vk_token = getenv('VK_TOKEN')
-    # environ['GOOGLE_APPLICATION_CREDENTIALS'] = getenv('GOOGLE_CRED')
+    environ['GOOGLE_APPLICATION_CREDENTIALS'] = getenv('GOOGLE_CRED')
     dialogflow_project_id = getenv('DIALOG_FLOW_ID')
     telebot_token = getenv('TELEGRAM_BOT_TOKEN')
     logger_chat_id = getenv('LOGGER_CHAT_ID')
